c553d05a2b23825f8a1e29bad9a08fc58134c2c6
ManojBR105 <manojbr105@gamil.com>
2025-08-18 09:24:09 -0700

    Fix resource usage limits in the automation tool

--- automation_tool/src/fpgas.py
+++ automation_tool/src/fpgas.py
@@ -3,7 +3,7 @@ from commons import FPGA, FPGAResource, HBM
 U280 = FPGA(
     available = FPGAResource(bram=2016, uram=960, dsp=9024, lut=1303680, reg=2607360), # Total Available Resources
     fixed = FPGAResource(bram=195.5, uram=0, dsp=4, lut=107629, reg=136086), # Resources not available to the user
-    limit = FPGAResource(bram=0.75, uram=0.5, dsp=0.5, lut=0.62, reg=0.62), # Utilization ratio limit (utilized + fixed)/available <= limit
+    limit = FPGAResource(bram=0.75, uram=0.75, dsp=0.75, lut=0.62, reg=0.62), # Utilization ratio limit (utilized + fixed)/available <= limit
     # HBM has 256b x 32 Channels @ 450MHZ
     # Assuming that we can acheive a kernel frequency of 225MHz, we have 32 x 512b @ 225MHz ~= 460GBPS
     # But realistically HLS designs can only acheive good frequency if the HBM channels are below 28
@@ -15,13 +15,13 @@ U280 = FPGA(
 U50 = FPGA(
     available = FPGAResource(bram=1344, uram=640, dsp=5952, lut=870720, reg=1743360), # Total Available Resources
     fixed = FPGAResource(bram=179.5, uram=4, dsp=4, lut=112760, reg=142875), # Resources not available to the user
-    limit = FPGAResource(bram=0.75, uram=0.5, dsp=0.5, lut=0.70, reg=0.70), # Utilization ratio limit (utilized + fixed)/available <= limit
+    limit = FPGAResource(bram=0.75, uram=0.75, dsp=0.75, lut=0.70, reg=0.70), # Utilization ratio limit (utilized + fixed)/available <= limit
     hbm=HBM(num_ch=28, ch_width=512, freq=225_000_000), # HBM Specs
     platform='xilinx_u50_gen3x16_xdma_5_202210_1',
     series='Ultrascale+'
 )
 
-# Predivtion for Versal HBM sereies
+# Prediction for Versal HBM sereies
 # References 
 # https://docs.amd.com/v/u/en-US/ds950-versal-overview Table 16 Column VH1782
 # https://www.amd.com/en/products/accelerators/alveo/v80/a-v80-p64g-pq-g.html
@@ -32,7 +32,7 @@ V80 = FPGA(
     # Refenece https://0x04.net/~mwk/xidocs/am/am004-versal-dsp-engine.pdf Figure 33: Floating Point Multiplier and Adder (DSPFP32 Mode)
     available = FPGAResource(bram=3741, uram=1925, dsp=10848,  lut=2574208, reg=5148416), 
     fixed = FPGAResource(bram=195.5, uram=0, dsp=4, lut=107629, reg=136086), # Assume same as U280
-    limit = FPGAResource(bram=0.75, uram=0.5, dsp=0.5, lut=0.70, reg=0.70),
+    limit = FPGAResource(bram=0.75, uram=0.75, dsp=0.75, lut=0.70, reg=0.70),
     # HBM has 128b x 16 channels @ 3.2GHz 
     # Assuming Max Kernel Freuency of 400MHz we get 256b x 64 channels @ 400MHz ~= 819.2GBPS
     # Assuming we can use all the ports
