e2777debd22720a8aa5e999c60c4104df8dfb821
ManojBR105 <manojbr105@gamil.com>
2025-08-22 23:10:47 -0700

    Update CPU config in benchmarking script to use different cores for each thread

--- cpu/env.sh
+++ cpu/env.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=$CONDA_LOC/pkgs/mkl-2025.0.0-hacee8c2_941/lib:$LD_LIBRARY_PATH
-export MKL_NUM_THREADS=24      # all cores
 export OMP_NUM_THREADS=24
-export KMP_AFFINITY=granularity=fine,compact,1,0
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
